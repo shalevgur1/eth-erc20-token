b1526c7d19773c306e8f751e964902864a97052d
Gur Shalev <shalevgur1@gmail.com>
2024-09-22 02:16:15 +0300

    add Erc20

--- contracts/CocoToken.sol
+++ contracts/CocoToken.sol
@@ -3,3 +3,10 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract CocoToken is ERC20 {
+
+    constructor (uint256 initialSupply) ERC20("CocoToken", "CCT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
